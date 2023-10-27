ca865300a7bd63eba85e8b3e243c3fe602d8b7b3
hayden.rear <hayden.rear@gmail.com>
2023-10-27 13:49:24 -0400

    updated

---
+++ chrome_streamer_be/src/lib.rs
@@ -0,0 +1,16 @@
+
+use js_sys::Array;
+use wasm_bindgen::prelude::*;
+use web_sys::HtmlElement;
+
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+
+pub fn start() {
+
+}
